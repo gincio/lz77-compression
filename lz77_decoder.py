d9568ceb0110263bdade26ce5d178c802017309f
gincio <hubert.a.adamkiewicz@gmail.com>
2021-01-21 23:04:13 +0100

    Decoder: processing input args, preventing file overwrite, verbose argument, tool help section with argparse

--- lz77_decoder.py
+++ lz77_decoder.py
@@ -1,30 +1,49 @@
+from argparse import ArgumentParser
+import os.path
+
 def main():
-    input_file = open('encoded.lz77', 'rb')
-    input_data = input_file.read()
-    input_file.close()
+    # Parse input parameters
+    parser = ArgumentParser(description='A tool decoding files encoded with lz77 algorithm (especially with encoder delivered with this tool)')
+    parser.add_argument("encoded_file_path", nargs=1, help='path to file encoded with lz77 algorithm')
+    parser.add_argument("output_file_path", nargs=1, help='path to store decoded data')
+    parser.add_argument('-v', '--verbose', action='store_true', help='an optional argument to show each step of encoding and additional info')
+    args = parser.parse_args()
+    if args.verbose:
+        print(f"Input args: \n\t \
+        Encoded file: {args.encoded_file_path}\n\t \
+        Output file: {args.output_file_path}\n\t")
+
+    # check if output file exists
+    if os.path.isfile(args.output_file_path[0]):
+        askForOverwrite(args.output_file_path[0])
+
+    # get encoded data
+    with open(args.encoded_file_path[0], 'rb') as input_file:
+        input_data = input_file.read()
     bin_data = bytesToBinaryString(input_data)
+
+    # init decoded data store
     decoded_data = []
-    for i in range(0, len(bin_data), 40):
-        data_chunk = bin_data[i:i+40]
-        offset1 = int(data_chunk[0:8], 2)
-        length1 = int(data_chunk[8:12], 2)
-        next_char1 = int(data_chunk[12:20], 2)
-        offset2 = int(data_chunk[20:28], 2)
-        length2 = int(data_chunk[28:32], 2)
-        next_char2 = int(data_chunk[32:40], 2)
-        #print(f"<{offset1}, {length1}, {next_char1}>\n<{offset2}, {length2}, {next_char2}>")
-        # TODO: FIX saving decoded file error!! Files are not the same!
-        for j in range(length1):
-            decoded_data.append(decoded_data[-offset1 - 1])
-        decoded_data.append(next_char1)
-        for j in range(length2):
-            decoded_data.append(decoded_data[-offset2 - 1])
-        decoded_data.append(next_char2)
-    print(f'{decoded_data}')
-    out_file = open('decoded.bmp', 'wb')
+
+    # start decoding
+    for i in range(0, len(bin_data), 20):
+        # get one lz77 code data chunk
+        data_chunk = bin_data[i:i+20]
+        offset = int(data_chunk[0:8], 2)
+        length = int(data_chunk[8:12], 2)
+        next_char = int(data_chunk[12:20], 2)
+
+        if args.verbose:
+            print(f"<{offset}, {length}, {next_char}>")
+
+
+        for j in range(length):
+            decoded_data.append(decoded_data[-offset - 1]) # append substring from search buffer
+        decoded_data.append(next_char) # append encoded character
+
+    out_file = open(args.output_file_path[0], 'wb')
     out_file.write(bytearray(decoded_data))
     out_file.close()
-        
         
 def bytesToBinaryString(data):
     stream_bytes = bytes(data)
@@ -34,4 +53,10 @@ def bytesToBinaryString(data):
     decoded_str = ''.join(string_arr)
     return decoded_str
     
+def askForOverwrite(path):
+    do_overwrite = input(f'File {path} exists. Do you want to overwrite it? [y/N]')
+    if (do_overwrite.lower() != 'y'):
+        print(f"\nYou have been chosen NOT to overwrite file. Try again with different filename. Exiting...")
+        exit(0)
+
 main()
